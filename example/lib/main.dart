9f3fb0e60c7d7765561ef8da6c7ce3cb1e60afd8
Denny <dev.dennysegura@gmail.com>
2018-12-07 15:37:59 -0300

    integration telegram, skype, twitter and linkedin bubble

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -3,6 +3,11 @@ import 'dart:async';
 
 import 'package:flutter/services.dart';
 import 'package:link_preview/link_preview.dart';
+import 'package:link_preview/widget/whatsapp/index.dart';
+import 'package:link_preview/widget/linkedin/index.dart';
+import 'package:link_preview/widget/skype/index.dart';
+import 'package:link_preview/widget/twitter/index.dart';
+import 'package:link_preview/widget/telegram/index.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,6 +18,12 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   LinkPreview previwer = LinkPreview();
+  WhatsAppLinkPreview wspPreview = WhatsAppLinkPreview();
+  LinkedInLinkPreview liPreview = LinkedInLinkPreview();
+  TwitterLinkPreview twPreview = TwitterLinkPreview();
+  TelegramLinkPreview tPreview = TelegramLinkPreview();
+  SkypeLinkPreview skPreview = SkypeLinkPreview();
+
   Future<String> _message = Future<String>.value('');
   @override
   void initState() {
@@ -22,7 +33,7 @@ class _MyAppState extends State<MyApp> {
   Future<String> initMetaDataState() async {
     dynamic linkPreview;
     try {
-      linkPreview = await previwer.getUrlMetaData(url: 'https://whatsapp.com');
+      linkPreview = await previwer.getUrlMetaData(url: 'https://flutter.io');
     } on PlatformException catch (e) {
       debugPrint(e.message);
       linkPreview = 'Failed to get platform version.';
@@ -34,31 +45,49 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Color.fromRGBO(241, 243, 244, 1.0),
         appBar: AppBar(
           title: const Text('Rich Link Preview example app'),
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            previwer.whatsAppLinkPreview(url: 'https://flutter.io'),
-            MaterialButton(
-              onPressed: () {
-                setState(() {
-                  _message = initMetaDataState();
-                });
-              },
-              textColor: Colors.white,
-              child: Text('Test Whatsapp Preview Metadata'),
-              color: Colors.blue,
-            ),
-            FutureBuilder<String>(
-              future: _message,
-              builder: (_, AsyncSnapshot<String> snapshot) {
-                return Text(snapshot.data ?? '');
-              },
-            ),
-          ],
+        body: SingleChildScrollView(
+          controller: ScrollController(),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                  margin: EdgeInsets.symmetric(vertical: 4.0),
+                  child: wspPreview.build('https://whatsapp.com')),
+              Container(
+                  margin: EdgeInsets.symmetric(vertical: 4.0),
+                  child: tPreview.build('https://telegram.org')),
+              Container(
+                  margin: EdgeInsets.symmetric(vertical: 4.0),
+                  child: skPreview.build('https://skype.com')),
+              Container(
+                  margin: EdgeInsets.symmetric(vertical: 4.0),
+                  child: twPreview.build('https://twitter.com')),
+              Container(
+                  margin: EdgeInsets.symmetric(vertical: 4.0),
+                  child: liPreview.build('https://linkedin.com')),
+              MaterialButton(
+                onPressed: () {
+                  setState(() {
+                    _message = initMetaDataState();
+                  });
+                },
+                textColor: Colors.white,
+                child: Text('Test Flutter Preview Metadata'),
+                color: Color.fromRGBO(19, 137, 253, 1.0),
+              ),
+              FutureBuilder<String>(
+                future: _message,
+                builder: (_, AsyncSnapshot<String> snapshot) {
+                  return Text(snapshot.data ?? '');
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
